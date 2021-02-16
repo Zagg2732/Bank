17d269e08ec53c3c5760e0265def33d79a038fd1
Zagg2732 <zagg2732@gmail.com>
2021-02-16 23:10:46 +0900

    find accounts 조건식 넣음. main 조금 수정

--- src/Bank.java
+++ src/Bank.java
@@ -11,19 +11,24 @@ public class Bank {
     HashMap<String , Account> map2 = new HashMap<String, Account>();
 
     public ArrayList<Account> findAccounts(String name) {
-        ArrayList<Account> myAccounts = new ArrayList<Account>();
+        if (map.containsValue(name)) {
+            System.out.println("= 해당 소유자명의 계좌정보 =");
+            ArrayList<Account> myAccounts = new ArrayList<Account>();
             //key가 이름이라 이에 해당하는 value값이 여러개일경우 다 반환해야하기 때문에 배열을 써야한다.
             //key로 get하면 Account가 value로 온다.
-        for(Map.Entry<Account, String> my : map.entrySet()) {
-            if (my.getValue().equals(name)) {
-                myAccounts.add(my.getKey());
+            for(Map.Entry<Account, String> my : map.entrySet()) {
+                if (my.getValue().equals(name)) {
+                    myAccounts.add(my.getKey());
+                }
             }
+            return myAccounts;
+        } else {
+            System.out.println("이름에 해당하는 계좌를 찾지못했습니다. 다시 확인해주세요");
+            return null;
         }
-        return myAccounts;
     }
 
     public Account getAccount(String accountNo) {
-        System.out.println("= 해당 계좌번호의 계좌정보 =");
         for(Map.Entry<String, Account> my : map2.entrySet()) {
             if (my.getKey().equals(accountNo)) {
                 return my.getValue();
@@ -33,6 +38,7 @@ public class Bank {
     }
 
     public ArrayList<Account> getAccounts() {
+        System.out.println("=은행에 등록된 모든 계좌를 조회합니다.=");
         return accounts;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,34 @@
 public class Main {
     public static void main(String[] args) {
         Bank woori = new Bank();
+
         woori.addAccount("0001", "이승준");
         woori.addAccount("0002", "이승준");
         woori.addAccount("0003", "이승준");
         woori.addAccount("0004", "김명환");
         woori.addAccount("0005", "김명환");
 
+        //입출금 테스트
+        woori.getAccount("0001").deposit(2000);
+        woori.getAccount("0001").withdraw(1000);
+        woori.getAccount("0001").withdraw(4000);
+
+
+        //모든 계좌 조회
+        System.out.println(woori.getAccounts());
+
         woori.getTotalAccount();
-        Object test = woori.findAccounts("이승준");
-        System.out.println(test);
-        Object test2 = woori.findAccounts("김명환");
-        System.out.println(test2);
+        System.out.println(woori.findAccounts("이승준"));
+        System.out.println(woori.findAccounts("이준"));
+        System.out.println(woori.findAccounts("김명환"));
+
 
         System.out.println(woori.getAccount("0003"));
 
         Bank gukmin = new Bank();
 
-        gukmin.getTotalAccount();
+        gukmin.getTotalAccount(); //0
+        //계좌만들기
         gukmin.addAccount("0001", "이순신");
         gukmin.addAccount("0002", "이순신");
         gukmin.addAccount("0002", "이순신");
