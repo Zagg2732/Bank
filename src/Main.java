f6f50534186e93bc82527bd45e1dda33f0ff3199
Zagg2732 <zagg2732@gmail.com>
2021-02-17 14:39:55 +0900

    main수정

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,30 @@ public class Main {
         woori.addAccount("0113", "택");
         woori.addAccount("987654321", "두팔");
 
-        System.out.println(woori.getAccounts());
-        System.out.println(woori.getAccount("890113"));
-
-        woori.getAccount("890113").deposit(200000);
-        woori.getAccount("890113").deposit(200000);
+        woori.getAccounts();
+        System.out.println();
 
         System.out.println(woori.getAccount("890113"));
+        System.out.println();
+        woori.getAccount("890113").deposit(2000000);
+        System.out.println();
+        System.out.println(woori.getAccount("890113"));
+
+
+        System.out.println();
+        woori.getAccount("890113").deposit(2000000);
+        System.out.println(woori.getAccount("890113"));
+
+        System.out.println();
+        System.out.println(woori.getAccount("10071"));
+        System.out.println();
 
         System.out.println(woori.findAccounts("택"));
-        woori.getAccount("890113");
+        System.out.println();
+
         woori.getAccount("890113").withdraw(5500);
+        System.out.println();
+        System.out.println(woori.getAccount("890113"));
 
         System.out.println(woori.getAccount("890113").getTransactions());
 
